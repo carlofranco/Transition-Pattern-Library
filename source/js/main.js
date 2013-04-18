ed826fc64d9cc0d7b005e195c84ba4f796bdef25
Kevin Finlayson <kevin@kevinfinlayson.com>
2013-04-17 18:07:43 -0700

    global variables and programmatic transition function

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,23 @@
-//= require_tree .
+var defaults = {
+        duration: 400,
+        easing: ''
+      },
+      white = '#fff',
+      light = '#eceded',
+      medium = '#d9dbdc',
+      dark = '#808181',
+      darkest = '#222',
+      text = '#4e4e4e',
+      title = '#ccc';
+
+$(document).ready(function(){
+
+  $.fn.transition = function (properties, options) {
+    options = $.extend({}, defaults, options);
+    properties.webkitTransition = 'all ' + options.duration + 'ms ' + options.easing;
+    $(this).css(properties);
+  };
+
+  
+
+});
